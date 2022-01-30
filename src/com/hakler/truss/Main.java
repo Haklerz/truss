bac9ced8b67e9a5fd7543ed3a1171b3821fcc96b
Haklerz <lervik.haakon@gmail.com>
2022-01-30 12:03:36 +0100

    feat: Add a application window

---
+++ src/com/hakler/truss/Main.java
@@ -0,0 +1,13 @@
+package com.hakler.truss;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Truss");
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
