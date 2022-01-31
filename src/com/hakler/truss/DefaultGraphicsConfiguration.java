a174b1ce5eb272c4626bb3a2ddf5164abf9ce61a
Haklerz <lervik.haakon@gmail.com>
2022-01-31 21:58:44 +0100

    refactor: Move default GraphicsConfig to singleton

---
+++ src/com/hakler/truss/DefaultGraphicsConfiguration.java
@@ -0,0 +1,19 @@
+package com.hakler.truss;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+
+public class DefaultGraphicsConfiguration {
+    private static GraphicsConfiguration instance;
+
+    public static GraphicsConfiguration getInstance() {
+        if (instance == null) {
+            instance = GraphicsEnvironment
+                    .getLocalGraphicsEnvironment()
+                    .getDefaultScreenDevice()
+                    .getDefaultConfiguration();
+        }
+
+        return instance;
+    }
+}
--- src/com/hakler/truss/Window.java
+++ src/com/hakler/truss/Window.java
@@ -2,30 +2,26 @@ package com.hakler.truss;
 
 import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.GraphicsEnvironment;
 
 import javax.swing.JFrame;
 
 public class Window {
     public Window(String title, int width, int height) {
         JFrame frame = new JFrame(title);
-
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        Canvas canvas = new Canvas(GraphicsEnvironment
-            .getLocalGraphicsEnvironment()
-            .getDefaultScreenDevice()
-            .getDefaultConfiguration());
+        Canvas canvas = new Canvas(DefaultGraphicsConfiguration.getInstance());
 
         canvas.setIgnoreRepaint(true);
         canvas.setBackground(Color.BLACK);
         canvas.setSize(width, height);
-        
+
         frame.add(canvas);
+
         frame.pack();
         frame.setMinimumSize(frame.getSize());
-
         frame.setLocationRelativeTo(null);
+
         frame.setVisible(true);
     }
 }
