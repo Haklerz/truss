bc391624b6bbc2c2fd477ea52648caa8b22de058
Haklerz <lervik.haakon@gmail.com>
2022-02-01 20:52:43 +0100

    refactor: Remove width and height fields

--- src/com/hakler/truss/FrameBuffer.java
+++ src/com/hakler/truss/FrameBuffer.java
@@ -6,17 +6,24 @@ import java.awt.image.BufferedImage;
 public class FrameBuffer {
 
     private BufferedImage image;
+    private int width, height;
 
     public FrameBuffer(int width, int height) {
-        image = DefaultGraphicsConfiguration.getInstance()
+        this.image = DefaultGraphicsConfiguration.getInstance()
                 .createCompatibleImage(width, height, Transparency.OPAQUE);
+        this.width = width;
+        this.height = height;
     }
 
     public int getWidth() {
-        return image.getWidth();
+        return width;
     }
 
     public int getHeight() {
-        return image.getHeight();
+        return height;
+    }
+
+    public BufferedImage asBufferedImage() {
+        return image;
     }
 }
